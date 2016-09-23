775aae0bde1e7fce46b4be4edbda344bcda524a8
Tiago Silva <tiago@tiagosilva.pt>
2016-09-23 17:22:28 +0100

    Create Alchemy for Restaurateurs test page

---
+++ wp-content/themes/twentythirteen-child/page-1181.php
@@ -0,0 +1,20 @@
+<?php /* ALCHEMY FOR RESTAURATEURS */ ?>
+<?php get_header(); ?>
+
+    <div class="contact_wrap">
+        <div class="table">
+            <div class="cell">
+                <div class="contact_wrap_inside">
+                    <div class="contact_content">
+                        [email-download download_id="1178" contact_form_id="1174"]
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <?php include('footer-nav.php'); ?>
+    </div>
+
+</div><!-- .wrapper -->
+
+<?php get_footer(); ?>
