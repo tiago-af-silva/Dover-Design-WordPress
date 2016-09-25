621c2d16c2bda0da5d184f664c992c40687e1e9b
Tiago Silva <tiago@tiagosilva.pt>
2016-09-25 23:42:06 +0100

    Update alchemy page

--- wp-content/themes/twentythirteen-child/page-1181.php
+++ wp-content/themes/twentythirteen-child/page-1181.php
@@ -4,7 +4,7 @@
     <div class="alchemy_wrap">
         <div class="table">
             <div class="cell">
-                <div class="alchemy_wrap_inside">
+                <div class="alchemy_wrap_content">
                     <h1>Sign up for your free copy of &lsquo;Alchemy for Restaurateurs&rsquo;</h1>
 
                     <?php echo do_shortcode('[email-download download_id="1178" contact_form_id="1174"]'); ?>
