47b0ddf3cdc39d613dcf00153dee355afe062945
Tiago Silva <tiago@tiagosilva.pt>
2016-09-25 23:12:39 +0100

    Alchemy page

--- wp-content/themes/twentythirteen-child/page-1181.php
+++ wp-content/themes/twentythirteen-child/page-1181.php
@@ -1,13 +1,13 @@
 <?php /* ALCHEMY FOR RESTAURATEURS */ ?>
 <?php get_header(); ?>
 
-    <div class="contact_wrap">
+    <div class="alchemy_wrap">
         <div class="table">
             <div class="cell">
-                <div class="contact_wrap_inside">
-                    <div class="contact_content">
-                        <?php echo do_shortcode('[email-download download_id="1178" contact_form_id="1174"]'); ?>
-                    </div>
+                <div class="alchemy_wrap_inside">
+                    <h1>Sign up for your free copy of &lsquo;Alchemy for Restaurateurs&rsquo;</h1>
+
+                    <?php echo do_shortcode('[email-download download_id="1178" contact_form_id="1174"]'); ?>
                 </div>
             </div>
         </div>
