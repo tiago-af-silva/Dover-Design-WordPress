382f33411021a02c86c9ef3d6f6fef2cab4396c0
Tiago Silva <tiago@tiagosilva.pt>
2017-03-15 01:27:58 +0000

    Implement designs for the news page

--- wp-content/themes/twentythirteen-child/page-1213.php
+++ wp-content/themes/twentythirteen-child/page-1213.php
@@ -1,42 +1,71 @@
 <?php /* NEWS */ ?>
 <?php get_header(); ?>
 
-    <?php
-        $args = array(
-            'numberposts' => '5',
-            'post_type' => 'newsletter',
-        );
-
-        $recent_posts = wp_get_recent_posts($args);
-
-        while (have_posts()) { the_post();
-    ?>
-        <div class="about_wrap">
-            <div class="about_wrap_inside">
-                <?php if (count($recent_posts) > 0) { ?>
-                    <div class="mailchimp-latest">
-                        <iframe src="<?php echo trim($recent_posts[0]['post_content']) ?>"></iframe>
-                    </div>
-                <?php } ?>
-
-                <?php
-                    array_shift($recent_posts);
-
-                    if (count($recent_posts) > 0) {
-                ?>
-                    <div class="mailchimp-archive">
-                        <ul>
-                            <?php foreach ($recent_posts as $recent_post) { ?>
-                                <li>
-                                    <a href="<?php echo trim($recent_post['post_content']) ?>" target="_blank"><?php echo $recent_post['post_title'] ?></a>
-                                </li>
-                            <?php } ?>
-                        </ul>
-                    </div>
-                <?php } ?>
+    <?php while (have_posts()) { the_post(); ?>
+        <div class="news_wrap">
+            <div class="news_title">
+                <h1>News</h1>
             </div>
 
-            <?php include('footer-nav.php'); ?>
+            <div class="news_content clearfix">
+                <?php
+                    $args = array(
+                        'numberposts' => '5',
+                        'post_type' => 'newsletter',
+                    );
+
+                    $recent_posts = wp_get_recent_posts($args);
+
+                    $share_message = urlencode('Latest news from Dover Design');
+
+                    foreach ($recent_posts as $recent_post) {
+                        $share_url = urlencode(trim($recent_post['post_content']));
+                ?>
+                    <div class="news_article">
+                        <div class="news_article_top clearfix">
+                            <div class="news_article_top_left">
+                                <a href="https://twitter.com/home?status=<?php echo $share_message ?>%20<?php echo $share_url ?>" title="Share on Twitter" target="_blank">
+                                    <?php echo file_get_contents('wp-content/themes/twentythirteen-child/assets/img/twitter.svg') ?>
+                                </a>
+                                <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $share_url ?>" title="Share on Facebook" target="_blank">
+                                    <?php echo file_get_contents('wp-content/themes/twentythirteen-child/assets/img/facebook.svg') ?>
+                                </a>
+                                <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo $share_url ?>&title=<?php echo $share_message ?>&summary=&source=" title="Share on LinkedIn" target="_blank">
+                                    <?php echo file_get_contents('wp-content/themes/twentythirteen-child/assets/img/linkedin.svg') ?>
+                                </a>
+                            </div>
+
+                            <div class="news_article_top_right">
+                                <a href="<?php echo trim($recent_post['post_content']) ?>" target="_blank">View post</a>
+                            </div>
+                        </div>
+
+                        <div class="news_article_meta">
+                            <h3 class="news_article_title"><?php echo $recent_post['post_title'] ?></h3>
+                            <p><?php echo get_the_date('F j, Y', $recent_post['ID']) ?></p>
+                        </div>
+
+                        <div class="news_article_content">
+                            <?php
+                                $dom = new DOMDocument();
+                                @$dom->loadHTMLFile(trim($recent_posts[0]['post_content']));
+                                $node = $dom->getElementById('bodyTable');
+
+                                $innerHTML = '';
+                                $children = $node->childNodes;
+
+                                foreach ($children as $child) { 
+                                    $innerHTML .= $child->ownerDocument->saveXML($child); 
+                                } 
+
+                                echo $innerHTML;
+                            ?>
+                        </div>
+                    </div>
+                <?php } ?>
+
+                <?php include('footer-nav.php'); ?>
+            </div>
         </div>
     <?php } ?>
 
