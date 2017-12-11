ad2978c76491fd9309832a2e19385c63a09b187a
Tiago Silva <tiago@tiagosilva.pt>
2017-12-11 23:29:24 +0000

    Stop version update checks for active theme

--- wp-content/themes/twentythirteen-child/functions.php
+++ wp-content/themes/twentythirteen-child/functions.php
@@ -286,3 +286,17 @@ if ($current_user->ID != $superadmin_id) {
         remove_meta_box('pageparentdiv', 'page', 'side');
     }
 }
+
+// Stop version update checks for this theme
+function stop_theme_version_check($r, $url) {
+    if (0 !== strpos( $url, 'http://api.wordpress.org/themes/update-check')) {
+        return $r; // Not a theme update request. Bail immediately.
+    }
+
+    $themes = unserialize($r['body']['themes']);
+    unset($themes[get_option('template')]);
+    unset($themes[get_option('stylesheet')]);
+    $r['body']['themes'] = serialize($themes);
+    return $r;
+}
+add_filter('http_request_args', 'stop_theme_version_check', 5, 2);
